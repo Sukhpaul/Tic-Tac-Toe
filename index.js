8942ccfc3266de9ffd92a8a87de2d51fbe6b93b9
Sukhpaul Nijjar <sukhpaul.nijjar@gmail.com>
2018-01-08 14:52:19 -0800

    Create inital scaffolding

---
+++ index.js
@@ -0,0 +1,24 @@
+// create 3x3 board
+// 1 = X, 2 = O
+// 2 players go turn by turn, informed whos turn it is 
+
+let Board = () => {
+  this.board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+  ]
+}
+
+Board.prototype = {
+  move: () => {
+    // allow player to make move
+  },
+
+  check: () => {
+    // check if any player has winning results
+  }
+}
+
+let x = new Board();
+console.log(x)
